9235262ac3ae6d5d22f688f5014d947f1466788f
vBelyi <slava.belyi16@gmail.com>
2023-01-31 19:26:23 +0200

    added dict5

--- mane.py
+++ mane.py
@@ -7,4 +7,5 @@ dict1 = {'Audi': 'A3', 'Mercedes': 'A-Class', 'BMW': 1}
 dict2 = {'Renault': 'Megan', 'Pegeout': '505'}
 dict3 = {'Dacia': 'Logan', 'Volvo': 'S60'}
 dict4 = {'Apple': 'fruit', 'Potato': 'vegetable'}
+dict5 = {'Rabbit':'animal', 'Jake': 'human' }
 print(mane_function(dict1, dict2, dict3, dict4))
