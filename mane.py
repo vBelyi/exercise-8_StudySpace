a85c16f7dcf8ee066260c7a40adbe5e2ea96d5ae
vBelyi <slava.belyi16@gmail.com>
2023-01-31 19:53:30 +0200

    added month 9,10,11 in test func

--- mane.py
+++ mane.py
@@ -9,3 +9,15 @@ dict3 = {'Dacia': 'Logan', 'Volvo': 'S60'}
 dict4 = {'Apple': 'fruit', 'Potato': 'vegetable'}
 dict5 = {'Rabbit':'animal', 'Jake': 'human' }
 print(mane_function(dict1, dict2, dict3, dict4, dict5))
+
+def test_function(month):
+    if month == 1 or month == 2 or month == 12:
+        return 'Winter'
+    elif month == 3 or month == 4 or month == 5:
+        return 'Spring'
+    elif month == 6 or month == 7 or month == 8:
+        return 'Summer'
+    elif month == 9 or month == 10 or month == 11:
+    else:
+        return 'There is only 12 monthes in the year'
+print(test_function(int(input('Click here to input a number of month to know a time of the year: '))))
